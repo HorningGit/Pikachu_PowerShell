8cf910e97a960559a22411bdbcca0d00d30bf794
AlexanderHorning <42924284+HorningGit@users.noreply.github.com>
2022-05-05 09:20:11 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,10 @@
-Get-ChildItem
+# Alexander Horning
+# Script for System Information
+
+# BIOS Info
+Write-Host "[BIOS Details]"
+Get-CimInstance -ClassName Win32_BIOS
+
+# Processor Info
+Write-Host "[Processor Details]"
+Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
